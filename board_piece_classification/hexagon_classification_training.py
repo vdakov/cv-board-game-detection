7177a1b94f87078be1be1fd27035471f8162457d
Elena-Oana Milchi <E.O.Milchi@student.tudelft.nl>
2025-03-24 13:02:05 +0100

    Add train, validation, test splits

--- board_piece_classification/hexagon_classification_training.py
+++ board_piece_classification/hexagon_classification_training.py
@@ -7,22 +7,26 @@ import pickle
 import tensorflow as tf
 from PIL import Image
 import numpy as np
-from tensorflow.python.keras.layers import MaxPool2D
 from torchvision.transforms import ToTensor
 
-
 def plot_hist(hist):
-    fig, axes = plt.subplots(1, 2)
+    fig, axes = plt.subplots(1, 2, figsize=(10, 5))
 
-    axes[0].plot(hist.history["accuracy"])
-    axes[0].set_title(f"Training accuracy for model: ")
+    # Plot train and validation accuracies
+    axes[0].plot(hist.history["accuracy"], label="Train accuracy", linestyle='solid', color='b')
+    axes[0].plot(hist.history["val_accuracy"], label="Validation accuracy", linestyle='dashed', color='c')
+    axes[0].set_title(f"Accuracies for CNN model on Catan tile set")
     axes[0].set_ylabel("Accuracy")
     axes[0].set_xlabel("Epoch")
+    axes[0].legend()
 
-    axes[1].plot(hist.history["loss"])
-    axes[1].set_title(f"Training loss for model: ")
+    # Plot train and validation losses
+    axes[1].plot(hist.history["loss"], label="Train loss", linestyle='solid', color='b')
+    axes[1].plot(hist.history["val_loss"], label="Validation loss", linestyle='dashed', color='c')
+    axes[1].set_title(f"Losses for model on Catan tile set")
     axes[1].set_ylabel("Loss")
     axes[1].set_xlabel("Epoch")
+    axes[1].legend()
 
     plt.tight_layout()
     plt.show()
@@ -33,6 +37,9 @@ def to_tensor(tensor_str):
     return torch.tensor(tensor_list)
 
 def model_eval(model, ds_test):
+
+    ds_test = ds_test.batch(BATCH_SIZE)
+
     loss, acc = model.evaluate(ds_test, verbose=2)
     print(f'Model loss on the test dataset: {loss}')
     print(f'Model accuracy on the test set: {acc}')
@@ -52,12 +59,16 @@ def model_predict(model, sample, label_encoder, img_size):
 
     print(f'Image at path: {sample} is a {pred_label} tile')
 
-def model_training(model, X, y, epochs):
+def model_training(model, train_set, valid_set, epochs):
     # Stop when the loss does not improve significantly over 3 epochs
     callback = callbacks.EarlyStopping(monitor='loss', min_delta=0.01, patience=3)
 
+    # Keras expects a batched dataset
+    batched_train = train_set.batch(BATCH_SIZE)
+    batched_valid = valid_set.batch(BATCH_SIZE)
+
     # Fit the model to the training data
-    hist = model.fit(x=X, y=y, epochs=epochs, callbacks=[callback])
+    hist = model.fit(x=batched_train, validation_data=batched_valid,  epochs=epochs, callbacks=[callback])
 
     # Plot training loss and accuracy
     plot_hist(hist)
@@ -101,32 +112,44 @@ if __name__ == "__main__":
 
     ##### PARAMETER DEFINITION #####
 
-    # EfficientNetB0 expects images of shape 224 x 224
     IMG_SIZE = (135, 121, 3)
     BATCH_SIZE = 64
     NUM_CLASSES = 6  # there are six tile types in Catan
-    epochs = 100 # the number of epochs used to train the model
-    dataset_path = '../data/sample/labeled_synthetic_samples.pkl'
-    path_to_predict = '../data/sample/test2.png'
-    model_save_path = '../data/sample/tile_detector.keras'
+    epochs = 100 # the maximum number of epochs used to train the model
+    validation_split = 0.2
+    test_split = 0.1
+    train_set_path = '../data/full/compiled_dataset/labeled_synthetic_samples.pkl'
+    path_to_predict = '../data/sample/test.jpg'
+    model_save_path = '../board_piece_classification/model/tile_detector.keras'
 
     ##### DATASET PRE-PROCESSING #####
 
-    with open(dataset_path, 'rb') as f:
-        X_data, y_labels, label_encoder = pickle.load(f)
+    with open(train_set_path, 'rb') as f:
+        X, y, label_encoder = pickle.load(f)
 
-    # Convert to TensorFlow tensors if needed
-    X_tensorflow = tf.convert_to_tensor(X_data, dtype=tf.float32)
-    y_tensorflow = tf.convert_to_tensor(y_labels, dtype=tf.int32)
+    # Convert to TensorFlow tensors
+    X = tf.convert_to_tensor(X, dtype=tf.float32)
+    y = tf.convert_to_tensor(y, dtype=tf.int32)
+
+    dataset = tf.data.Dataset.from_tensor_slices((X, y))
+    dataset = dataset.shuffle(buffer_size=len(X))
+
+    test_size = int(test_split * len(X))
+    validation_size = int(validation_split * len(X))
+    train_size = len(X) - test_size - validation_size
+
+    train_set = dataset.take(train_size)
+    validation_set = dataset.skip(validation_size)
+    test_set = dataset.skip(test_size)
 
     ##### TRAINING THE MODEL #####
 
     # First build the model
     model = build_cnn()
 
-    # Then train it on our input data
+    # Then train it on the input data
     # This will also plot the train and validation accuracies
-    model = model_training(model, X_tensorflow, y_tensorflow, epochs)
+    model = model_training(model, train_set, validation_set, epochs)
 
     model.save(model_save_path)
     print(f'Trained model saved at: {model_save_path}')
@@ -134,4 +157,7 @@ if __name__ == "__main__":
     ##### TESTING THE MODEL #####
 
     # Test the model on the test set
+    model_eval(model, test_set)
+
+    # Predict a single sample that is different from the test set
     model_predict(model, path_to_predict, label_encoder, IMG_SIZE)
