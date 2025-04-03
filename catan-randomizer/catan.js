0fd2d6bbf4f63bdb16e52417278ae79d3025937e
AlpiGun5 <a.i.guncan02@gmail.com>
2025-04-03 13:23:13 +0200

    Implement data augmentation with varying tile type sprites

---
+++ catan-randomizer/catan.js
@@ -0,0 +1,757 @@
+// ----- Canvas globals -----
+
+var mapCanvas;
+var drawingContext;
+
+var canvasCenterX;
+var canvasCenterY;
+
+// ----- Hexagon drawing parameters -----
+
+var mapStyle = "retro";
+
+var size = null;
+var defaultFillStyle = "#ffffff";
+var strokeStyle = "#000000";
+var lineWidth = 3;
+var resourceTypeToColor = {
+	"ore": "#363636",
+	"clay": "#E83200",
+	"wool": "#98E82E",
+	"wood": "#0A7300",
+	"grain": "#E0E000",
+	"desert": "#F2F0A0",
+	"none": "#ffffff"
+};
+var resourceTypeToImageCanvas = {
+	"ore": [],
+	"clay": [],
+	"wool": [],
+	"wood": [],
+	"grain": [],
+	"desert": []
+};
+
+// Store the selected image canvas for each tile
+var tileToImageCanvas = {};
+
+//var allImagesLoaded = false;
+
+// ----- Grid layout globals -----
+
+var dx = size * (1 + Math.cos(Math.PI / 3)) / 2;
+var dy = size * Math.sin(Math.PI / 3);
+
+/*
+ * Formula:
+ * 
+ * Height = (coordSpacing + 2) * dy
+ *        = (coordSpacing + 2) * Math.sin(Math.PI/3) * size
+ * Size = Height / ( (coordSpacing + 2) * Math.sin(Math.PI/3) )
+ * 
+ * Width = (coordSpacing * dx) + (2 * size)
+ *       = (coordSpacing * (1 + Math.cos(Math.PI/3)) / 2 * size) + (2 * size)
+ *       = ( (coordSpacing * (1 + Math.cos(Math.PI/3)) / 2) + 2 ) * size
+ * Size = Width / ( (coordSpacing * (1 + Math.cos(Math.PI/3)) / 2) + 2 )
+*/
+
+// ----- Map definition globals -----
+
+var catanMap = new CatanMap();
+
+var normalMap = new MapDefinition();
+normalMap.resourceDict = {
+	"desert": 1,
+	"wood": 4,
+	"clay": 3,
+	"wool": 4,
+	"grain": 4,
+	"ore": 3
+};
+normalMap.numberDict = {
+	2: 1,
+	3: 2,
+	4: 2,
+	5: 2,
+	6: 2,
+	8: 2,
+	9: 2,
+	10: 2,
+	11: 2,
+	12: 1
+}
+normalMap.coordinatesArray = [
+	[-4, 2], [-4, 0], [-4, -2],
+	[-2, 3], [-2, 1], [-2, -1], [-2, -3],
+	[0, 4], [0, 2], [0, 0], [0, -2], [0, -4],
+	[2, 3], [2, 1], [2, -1], [2, -3],
+	[4, 2], [4, 0], [4, -2]
+];
+
+var expandedMap = new MapDefinition();
+expandedMap.resourceDict = {
+	"desert": 2,
+	"wood": 6,
+	"clay": 5,
+	"wool": 6,
+	"grain": 6,
+	"ore": 5
+}
+expandedMap.numberDict = {
+	2: 2,
+	3: 3,
+	4: 3,
+	5: 3,
+	6: 3,
+	8: 3,
+	9: 3,
+	10: 3,
+	11: 3,
+	12: 2
+}
+expandedMap.coordinatesArray = [
+	[-6, 2], [-6, 0], [-6, -2],
+	[-4, 3], [-4, 1], [-4, -1], [-4, -3],
+	[-2, 4], [-2, 2], [-2, 0], [-2, -2], [-2, -4],
+	[0, 5], [0, 3], [0, 1], [0, -1], [0, -3], [0, -5],
+	[2, 4], [2, 2], [2, 0], [2, -2], [2, -4],
+	[4, 3], [4, 1], [4, -1], [4, -3],
+	[6, 2], [6, 0], [6, -2]
+];
+
+// ----- FUNCTIONS -----
+
+window.onresize = function (event) {
+	sizeCanvas();
+	catanMap.resize();
+	catanMap.draw();
+}
+
+function init() {
+
+	loadImages(function () {
+		var button = $('button#gen-map-button')[0];
+		$(button).click(generate);
+		button.disabled = false;
+		button.innerHTML = "Click to generate.";
+
+	});
+
+	addCanvas();
+
+}
+
+function preloadImages(arr, callback) {
+	//http://www.javascriptkit.com/javatutors/preloadimagesplus.shtml
+
+	var newimages = [], loadedimages = 0;
+	var postaction = function () { };
+	var arr = (typeof arr != "object") ? [arr] : arr;
+	function imageloadpost() {
+		loadedimages++;
+		if (loadedimages == arr.length) {
+			callback(newimages); //call postaction and pass in newimages array as parameter
+		}
+	}
+	for (var i = 0; i < arr.length; i++) {
+		newimages[i] = new Image();
+		newimages[i].src = arr[i];
+		newimages[i].onload = function () {
+			imageloadpost();
+		}
+		newimages[i].onerror = function () {
+			imageloadpost();
+		}
+	}
+
+}
+
+function loadImages(callback) {
+
+	var imgPaths = {};
+
+	// Scan for available images using a pattern: resourceType_X.png
+	// where X is a number starting from 1
+	for (var resourceType in resourceTypeToImageCanvas) {
+		imgPaths[resourceType] = [];
+		// Default image (original behavior)
+		imgPaths[resourceType].push("images/" + resourceType + ".png");
+		for (var i = 1; i <= 3; i++) {
+			imgPaths[resourceType].push("images/" + resourceType + "_" + i + ".png");
+		}
+	}
+
+	var totalImages = 0;
+	for (var key in imgPaths) {
+		totalImages += imgPaths[key].length;
+	}
+
+	var loadedImages = 0;
+
+	function onAllImagesLoaded() {
+		if (loadedImages >= totalImages) {
+			callback();
+		}
+	}
+
+	// Load all images for each resource type
+	for (var resourceType in imgPaths) {
+		for (var i = 0; i < imgPaths[resourceType].length; i++) {
+			var path = imgPaths[resourceType][i];
+			var img = new Image();
+			img.src = path;
+
+			// Use closures to capture the correct resourceType
+			(function (rt, image, imagePath) {
+				image.onload = function () {
+					var imgCanvas = document.createElement("canvas");
+					var imgContext = imgCanvas.getContext("2d");
+
+					imgCanvas.width = image.width;
+					imgCanvas.height = image.height;
+					imgContext.drawImage(image, 0, 0);
+
+					resourceTypeToImageCanvas[rt].push(imgCanvas);
+					loadedImages++;
+					onAllImagesLoaded();
+					console.log("Loaded: " + imagePath + " for " + rt);
+				};
+
+				image.onerror = function () {
+					// Don't log for variant images that might not exist
+					if (!imagePath.includes("_")) {
+						console.log("Failed to load: " + imagePath);
+					}
+					loadedImages++;
+					onAllImagesLoaded();
+				};
+			})(resourceType, img, path);
+		}
+	}
+}
+
+function generate() {
+
+	var mapDef;
+	switch ($("input:radio['name=game-type']:checked").val()) {
+		case "expanded":
+			mapDef = expandedMap;
+			break;
+		default:
+			mapDef = normalMap;
+	}
+
+	catanMap.defineMap(mapDef);
+	catanMap.generate();
+	catanMap.resize();
+	catanMap.draw();
+
+}
+
+function MapDefinition() {
+	this.resourceDict = null;
+	this.numberDict = null;
+	this.coordinatesArray = null;
+}
+MapDefinition.prototype.checkValidity = function () {
+	var cArrLen = this.coordinatesArray.length;
+	var rDictLen = this.sumDictVals(this.resourceDict);
+	var nDictLen = this.sumDictVals(this.numberDict);
+	var numDeserts = this.resourceDict["desert"];
+
+	return (cArrLen == rDictLen) && (rDictLen == (nDictLen + numDeserts));
+}
+MapDefinition.prototype.sumDictVals = function (dict) {
+	var sum = 0;
+	for (var key in dict) {
+		sum += dict[key];
+	}
+	return sum;
+}
+
+function CatanMap() {
+
+	this.mapDefinition = null;
+	this.hexTiles = null;
+	this.coordToTile = {};
+	this.coordSpan = [0, 0];
+
+}
+CatanMap.prototype.defineMap = function (mapDefinition) {
+
+	if (mapDefinition.checkValidity()) {
+
+		this.mapDefinition = mapDefinition;
+
+		var coordRangeX = [0, 0];
+		var coordRangeY = [0, 0];
+
+		for (var i = 0; i < mapDefinition.coordinatesArray.length; i += 1) {
+			var coord = mapDefinition.coordinatesArray[i];
+			coordRangeX = [
+				Math.min(coordRangeX[0], coord[0]),
+				Math.max(coordRangeX[1], coord[0])
+			];
+			coordRangeY = [
+				Math.min(coordRangeY[0], coord[1]),
+				Math.max(coordRangeY[1], coord[1])
+			];
+		}
+
+		this.coordSpan = [
+			coordRangeX[1] - coordRangeX[0],
+			coordRangeY[1] - coordRangeY[0]
+		];
+
+	} else {
+		console.log("Invalid map definition.");
+	}
+}
+CatanMap.prototype.generate = function () {
+
+	if (this.mapDefinition) {
+
+		this.hexTiles = [];
+
+		var numTiles = this.mapDefinition.coordinatesArray.length;
+
+		var tileCoordinates = this.mapDefinition.coordinatesArray.copy();
+
+		var tileNumbers = [];
+		for (var key in this.mapDefinition.numberDict) {
+			for (var i = 0; i < this.mapDefinition.numberDict[key]; i += 1) {
+				tileNumbers.push(parseInt(key));
+			}
+		}
+
+		var tileTypes = [];
+		for (var key in this.mapDefinition.resourceDict) {
+			if (key != "desert") {
+				for (var i = 0; i < this.mapDefinition.resourceDict[key]; i += 1) {
+					tileTypes.push(key);
+				}
+			}
+		}
+
+		var newCoords = null;
+		var numDeserts = this.mapDefinition.resourceDict["desert"];
+
+		// Clear the tile-to-image mapping
+		tileToImageCanvas = {};
+
+		for (var i = 0; i < numDeserts; i += 1) {
+			var desertHexTile = new HexTile();
+			newCoords = tileCoordinates.random(true);
+			desertHexTile.setCoordinate.apply(
+				desertHexTile,
+				newCoords
+			);
+			desertHexTile.setResourceType("desert");
+
+			// Select a random image for this tile if multiple are available
+			if (resourceTypeToImageCanvas["desert"].length > 0) {
+				var randomImageIndex = Math.floor(Math.random() * resourceTypeToImageCanvas["desert"].length);
+				tileToImageCanvas[newCoords.toString()] = resourceTypeToImageCanvas["desert"][randomImageIndex];
+			}
+
+			this.hexTiles.push(desertHexTile);
+			this.coordToTile[newCoords.toString()] = desertHexTile;
+		}
+
+		// Move all highly productive tile number (6 and 8) to the front
+		// of the tileNumbers array
+		var highlyProductiveIdx = [];
+		highlyProductiveIdx = highlyProductiveIdx.concat(
+			tileNumbers.indexOfArray(6),
+			tileNumbers.indexOfArray(8)
+		);
+		for (var i = 0; i < highlyProductiveIdx.length; i += 1) {
+			tileNumbers.swap(i, highlyProductiveIdx[i]);
+		}
+
+		// Handle all other tiles
+		for (var i = 0; i < (numTiles - numDeserts); i += 1) {
+
+			var newHexTile = new HexTile();
+			newHexTile.setNumber(tileNumbers[i]);
+			newHexTile.setResourceType(tileTypes.random(true));
+
+			var invalid;
+
+			if (newHexTile.isHighlyProductive()) {
+				var tmpCoords = [];
+				do {
+					newCoords = tileCoordinates.random(true);
+					newHexTile.setCoordinate.apply(
+						newHexTile,
+						newCoords
+					);
+					invalid = this.hasHighlyProductiveNeighbors(newHexTile);
+					if (invalid) {
+						tmpCoords.push(newCoords);
+					}
+				} while (invalid);
+				tileCoordinates = tileCoordinates.concat(tmpCoords);
+			} else {
+				newCoords = tileCoordinates.random(true);
+				newHexTile.setCoordinate.apply(
+					newHexTile,
+					newCoords
+				);
+			}
+
+			// Select a random image for this tile if multiple are available
+			if (resourceTypeToImageCanvas[newHexTile.resourceType].length > 0) {
+				var randomImageIndex = Math.floor(Math.random() * resourceTypeToImageCanvas[newHexTile.resourceType].length);
+				tileToImageCanvas[newCoords.toString()] = resourceTypeToImageCanvas[newHexTile.resourceType][randomImageIndex];
+			}
+
+			this.hexTiles.push(newHexTile);
+			this.coordToTile[newCoords.toString()] = newHexTile;
+		} // end for loop
+
+	} else {
+		console.log("No map definition.");
+	}
+
+}
+CatanMap.prototype.draw = function () {
+
+	if (this.hexTiles) {
+		drawingContext.clear();
+		for (var i = 0; i < this.hexTiles.length; i += 1) {
+			this.hexTiles[i].draw();
+		}
+	}
+
+}
+CatanMap.prototype.resize = function () {
+	/* Size = Height / ( (coordSpacing + 2) * Math.sin(Math.PI/3) )
+	 * Size = Width / ( (coordSpacing * (1 + Math.cos(Math.PI/3)) / 2) + 2 )
+	*/
+	var wSize = (mapCanvas.width - 10) /
+		((this.coordSpan[0] * (1 + Math.cos(Math.PI / 3)) / 2) + 2);
+	var hSize = (mapCanvas.height - 10) /
+		((this.coordSpan[1] + 2) * Math.sin(Math.PI / 3));
+	size = Math.floor(Math.min(wSize, hSize));
+	dx = size * (1 + Math.cos(Math.PI / 3)) / 2;
+	dy = size * Math.sin(Math.PI / 3);
+}
+CatanMap.prototype.getAdjacentTiles = function (tile) {
+
+	var tileX = tile.gridX;
+	var tileY = tile.gridY;
+
+	var adjTiles = [];
+
+	// (+0,+2), (+2,+1), (+2,-1), (+0,-2), (-2,-1), (-2,1)
+	xshift = [0, 2, 2, 0, -2, -2];
+	yshift = [2, 1, -1, -2, -1, 1];
+
+	for (var i = 0; i < 6; i += 1) {
+		var adjTile = this.coordToTile[
+			[tileX + xshift[i], tileY + yshift[i]].toString()
+		];
+		// Will be null if no hex tile found at that coordinate
+		if (adjTile) {
+			adjTiles.push(adjTile);
+		}
+	}
+
+	return adjTiles;
+
+}
+CatanMap.prototype.hasHighlyProductiveNeighbors = function (tile) {
+	var adjacentTiles = this.getAdjacentTiles(tile);
+	for (var i = 0; i < adjacentTiles.length; i += 1) {
+		if (adjacentTiles[i].isHighlyProductive()) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function HexTile() {
+	this.gridX;
+	this.gridY;
+	this.xCenter;
+	this.yCenter;
+	this.resourceType = "none";
+	this.fillStyle = defaultFillStyle;
+	this.number;
+}
+HexTile.prototype.strokeStyle = strokeStyle;
+HexTile.prototype.lineWidth = lineWidth;
+HexTile.prototype.hexColorMap = resourceTypeToColor;
+HexTile.prototype.size = size;
+HexTile.prototype.setResourceType = function (resourceType) {
+	if (this.hexColorMap[resourceType]) {
+		this.resourceType = resourceType;
+		this.fillStyle = this.hexColorMap[resourceType];
+	} else {
+		console.log("Unrecognized resource type:", resourceType);
+	}
+}
+HexTile.prototype.isHighlyProductive = function () {
+	return ((this.number == 6) || (this.number == 8));
+}
+HexTile.prototype.setNumber = function (number) {
+	this.number = number;
+}
+HexTile.prototype.setCoordinate = function (x, y) {
+	this.gridX = x;
+	this.gridY = y;
+}
+HexTile.prototype.draw = function () {
+	this.xCenter = canvasCenterX + dx * this.gridX;
+	this.yCenter = canvasCenterY + dy * this.gridY;
+
+	this.drawBase();
+	// Don't draw number if desert
+	if (this.number) {
+		this.drawNumber();
+	}
+}
+HexTile.prototype.drawBase = function () {
+
+	if (mapStyle == "retro") {
+		drawingContext.lineWidth = 10;
+		drawingContext.fillStyle = "rgba(255,255,255,0)";
+		drawingContext.strokeStyle = "#FAEB96";
+	} else {
+		drawingContext.lineWidth = this.lineWidth;
+		drawingContext.fillStyle = this.fillStyle;
+		drawingContext.strokeStyle = this.strokeStyle;
+	}
+
+	var angleOffset = Math.PI / 6;
+
+	// Begin Path and start at top of hexagon
+	drawingContext.beginPath();
+	drawingContext.moveTo(
+		this.xCenter + size * Math.sin(angleOffset),
+		this.yCenter - size * Math.cos(angleOffset)
+	);
+	// Move clockwise and draw hexagon
+	var newAngle;
+	for (var i = 1; i <= 6; i += 1) {
+		newAngle = i * Math.PI / 3;
+		drawingContext.lineTo(
+			this.xCenter + size * Math.sin(newAngle + angleOffset),
+			this.yCenter - size * Math.cos(newAngle + angleOffset)
+		);
+	}
+	drawingContext.closePath();
+
+	if (mapStyle == "retro") {
+		// Get the tile's coordinates as a string to look up in the tileToImageCanvas map
+		var tileCoord = [this.gridX, this.gridY].toString();
+		var imgCanvas;
+
+		// Use the assigned image if one exists, otherwise fall back to the first image
+		if (tileToImageCanvas[tileCoord]) {
+			imgCanvas = tileToImageCanvas[tileCoord];
+		} else if (resourceTypeToImageCanvas[this.resourceType].length > 0) {
+			imgCanvas = resourceTypeToImageCanvas[this.resourceType][0];
+		} else {
+			// Fallback if no images are available
+			drawingContext.fillStyle = this.fillStyle;
+			drawingContext.fill();
+			drawingContext.stroke();
+			return;
+		}
+
+		drawingContext.drawImage(
+			imgCanvas,
+			0, 0, imgCanvas.width, imgCanvas.height,
+			this.xCenter - size,
+			this.yCenter - dy,
+			2 * size,
+			2 * dy
+		);
+
+	} else {
+		drawingContext.fill();
+	}
+
+	drawingContext.stroke();
+
+}
+HexTile.prototype.drawNumber = function () {
+
+	drawingContext.fillStyle = "#FFFFFF";
+	drawingContext.strokeStyle = "#000000";
+	drawingContext.lineWidth = 3;
+
+	drawingContext.beginPath();
+	drawingContext.arc(this.xCenter, this.yCenter, 0.375 * size,
+		0, 2 * Math.PI, false);
+	drawingContext.closePath();
+
+	drawingContext.fill();
+	drawingContext.stroke();
+
+	var fontSizePt = Math.ceil(30 / 40 * (.45 * size - 8) + 6);
+
+	drawingContext.font = "bold " + fontSizePt + "pt sans-serif";
+	drawingContext.textAlign = "center";
+	if (this.isHighlyProductive()) {
+		drawingContext.fillStyle = "#FF0000";
+	} else {
+		drawingContext.fillStyle = "#000000";
+	}
+	drawingContext.fillText(
+		this.number.toString(),
+		this.xCenter,
+		this.yCenter + Math.ceil(0.85 * fontSizePt / 2)
+	);
+
+}
+
+Array.prototype.random = function (removeElem) {
+	var idx = Math.floor(Math.random() * this.length);
+	var val = this[idx];
+	if (removeElem) {
+		this.splice(idx, 1);
+	}
+	return val;
+}
+Array.prototype.copy = function () {
+	return this.slice();
+}
+Array.prototype.indexOfArray = function (val) {
+	var arr = [];
+	var sIdx = 0;
+	var tmpCopy = this.copy();
+	do {
+		var rIdx = tmpCopy.indexOf(val);
+		var valid = (rIdx >= 0);
+		if (valid) {
+			tmpCopy.splice(0, rIdx + 1);
+			arr.push(sIdx + rIdx);
+			sIdx += rIdx + 1;
+		}
+	} while (valid);
+	return arr;
+}
+Array.prototype.swap = function (idx1, idx2) {
+	var tmp = this[idx1];
+	this[idx1] = this[idx2];
+	this[idx2] = tmp;
+}
+
+function addCanvas() {
+	//$(mapCanvas).attr("width", 600);
+	//$(mapCanvas).attr("height", 400);
+	mapCanvas = document.createElement("canvas");
+	drawingContext = mapCanvas.getContext('2d');
+	mapCanvas.id = "map-canvas";
+
+	sizeCanvas();
+
+	document.getElementById("map-container").appendChild(mapCanvas);
+
+}
+
+function sizeCanvas() {
+	var mapContainer = $("div#map-container")[0];
+	$(mapCanvas).attr("width", $(mapContainer).width());
+	$(mapCanvas).attr("height", $(mapContainer).height());
+	canvasCenterY = mapCanvas.height / 2;
+	canvasCenterX = mapCanvas.width / 2;
+}
+
+// http://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing
+CanvasRenderingContext2D.prototype.clear =
+	CanvasRenderingContext2D.prototype.clear || function (preserveTransform) {
+		if (preserveTransform) {
+			this.save();
+			this.setTransform(1, 0, 0, 1, 0, 0);
+		}
+
+		this.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+		if (preserveTransform) {
+			this.restore();
+		}
+	};
+
+// Function to add additional image variants dynamically
+function addImageVariant(resourceType, imagePath, callback) {
+	if (!resourceTypeToImageCanvas[resourceType]) {
+		console.error("Invalid resource type: " + resourceType);
+		if (callback) callback(false);
+		return;
+	}
+
+	var img = new Image();
+	img.src = imagePath;
+
+	img.onload = function () {
+		var imgCanvas = document.createElement("canvas");
+		var imgContext = imgCanvas.getContext("2d");
+
+		imgCanvas.width = img.width;
+		imgCanvas.height = img.height;
+		imgContext.drawImage(img, 0, 0);
+
+		resourceTypeToImageCanvas[resourceType].push(imgCanvas);
+		console.log("Added new variant for " + resourceType + ": " + imagePath);
+
+		if (callback) callback(true);
+	};
+
+	img.onerror = function () {
+		console.error("Failed to load: " + imagePath);
+		if (callback) callback(false);
+	};
+}
+
+// Add a method to HexTile to set a specific image variant
+HexTile.prototype.setImageVariant = function (variantIndex) {
+	// Check if the resource type is valid and has the requested variant
+	if (resourceTypeToImageCanvas[this.resourceType] &&
+		resourceTypeToImageCanvas[this.resourceType].length > 0) {
+
+		// Ensure the index is within bounds
+		var index = variantIndex;
+		if (index >= resourceTypeToImageCanvas[this.resourceType].length) {
+			index = 0;
+			console.warn("Requested variant index " + variantIndex +
+				" exceeds available variants for " + this.resourceType +
+				". Using variant 0 instead.");
+		} else if (index < 0) {
+			index = 0;
+			console.warn("Invalid variant index: " + variantIndex +
+				". Using variant 0 instead.");
+		}
+
+		// Store the selected image canvas for this tile
+		var tileCoord = [this.gridX, this.gridY].toString();
+		tileToImageCanvas[tileCoord] = resourceTypeToImageCanvas[this.resourceType][index];
+
+		return true;
+	}
+
+	return false;
+}
+
+// Add a method to CatanMap to set a specific image variant for a specific tile
+CatanMap.prototype.setTileImageVariant = function (gridX, gridY, variantIndex) {
+	var tileCoord = [gridX, gridY].toString();
+	var tile = this.coordToTile[tileCoord];
+
+	if (tile) {
+		return tile.setImageVariant(variantIndex);
+	}
+
+	return false;
+}
+
+// Add a method to get the number of available variants for a resource type
+function getAvailableVariants(resourceType) {
+	if (resourceTypeToImageCanvas[resourceType]) {
+		return resourceTypeToImageCanvas[resourceType].length;
+	}
+	return 0;
+}
